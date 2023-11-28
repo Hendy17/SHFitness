5ac1501c1b18050c57b053914ff0c8cb14f59c7f
Hendy17 <hendyvorpagel@gmail.com>
2023-11-28 17:08:20 -0300

    primeiro commit

---
+++ backend/src/main/java/com/shfitness/shfitness/ShfitnessApplication.java
@@ -0,0 +1,13 @@
+package com.shfitness.shfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShfitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShfitnessApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shfitness/shfitness/ShfitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shfitness.shfitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShfitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
